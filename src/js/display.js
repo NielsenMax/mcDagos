4702a3a31e0094a90e61f2d613ba809df9a3531a
maxi nielsen <maxianielsen@gmail.com>
2019-09-06 11:19:15 -0300

    cambios en display

--- src/js/display.js
+++ src/js/display.js
@@ -1,24 +1,55 @@
 $(document).ready(function(){
+    dataNew = [];
+    dataOld = [];
+    function diffArray(pedidosA,pedidosB){
+        eliminables = [];
+        agregables = [];
+        pedidosA.forEach(
+            function (pa){
+                if(pedidosB.indexOf(pa)<0){
+                    eliminables.push(pa);
+                }
+            }
+        );
+        pedidosB.forEach(
+            function (pb){
+                if(pedidosA.indexOf(pb)<0){
+                    agregables.push(pb);
+                }
+            }
+        );
+        return {agregar: agregables, eliminar: eliminables};
+    }
     setInterval(function(){
         $.ajax({    
             type: "POST",
             url: "../php/readJSON.php",             
             dataType: "json",          
             success: function(response){  //response contains the json from data.json                  
-                output = ""; 
-                Object.keys(response).forEach(function(key) { //loop to every key in response
-                    //console.log(key, response[key]);
-                    // <div class="container"> 
-                    //     <div class="mipene">42235</div>
-                    //     <div><img src="/mcDagos/13-132538_burger-svg-png-icon-free-download-545640-clip.png" style="width:auto;height:100px" alt="Lights"></div>
-                    // </div>  
-                    if(response[key] == 1){
-                        output = output + '<div class="container"> <div class="mipene">' + key.toString() +'</div><div><img src="/mcDagos/13-132538_burger-svg-png-icon-free-download-545640-clip.png" style="width:auto;height:100px" alt="Lights"></div></div>';
-                    } else if (response[key] == 2){
-                        output = output + '<div class="container"> <div class="mipene">' + key.toString() +'</div><div>lista</div></div>';
+                // output = ""; 
+                // Object.keys(response).forEach(function(key) { //loop to every key in response
+                //     //console.log(key, response[key]);
+                //     // <div class="container"> 
+                //     //     <div class="mipene">42235</div>
+                //     //     <div><img src="/mcDagos/13-132538_burger-svg-png-icon-free-download-545640-clip.png" style="width:auto;height:100px" alt="Lights"></div>
+                //     // </div>  
+                //     if(response[key] == 1){
+                //         output = output + '<div class="container"> <div class="mipene">' + key.toString() +'</div><div><img src="/mcDagos/13-132538_burger-svg-png-icon-free-download-545640-clip.png" style="width:auto;height:100px" alt="Lights"></div></div>';
+                //     } else if (response[key] == 2){
+                //         output = output + '<div class="container"> <div class="mipene">' + key.toString() +'</div><div>lista</div></div>';
+                //     }
+                //});  
+                
+
+                Object.keys(response).forEach(function(key) {
+                    if(response[key]){
+                        dataNew.push(key);
                     }
-                });  
-                $("#display").html(output);
+                });
+                data = diffArray(dataOld, dataNew);
+                //hace cosas alejo
+
+                dataOld =dataNew;
             },
             error: function() {
                 console.log("Error del servidor");
