02ad2d0963b3a0bebbb57b510a540b30aa0b239c
Luis Aseguinolaza <luisasg_2001@hotmail.com>
2019-09-08 22:03:17 -0300

    Agregado Feedback y Imagen al cajero

--- src/php/save2JSON.php
+++ src/php/save2JSON.php
@@ -5,11 +5,27 @@ require_once('Mcbacka.php');
 use McDagos;
 
 $mcData = new Mcbacka();
+
+function nombre($i){ ///'1' cooking, '2' ready, '0' delivered
+	if($i == 1){
+		return "Cocinando";
+	}
+	if($i == 2){
+		return "Listo";
+	}
+	if($i == 0){
+		return "Entregado";
+	}
+	
+	
+}
+
+
 if($_POST['number']){
      if($mcData->loadData($_POST['number'], $_POST['estate'])){
-          echo "";
+          echo "<p style='color:green'> Orden ". $_POST['number'] . " => " . nombre( $_POST['estate'] ) . "</p>";
      }else{
-          echo '<p>Error</p>';
+          echo "<p style='color:red'>Error</p>";
      }
 }
 
